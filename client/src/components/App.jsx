99e84b62f52f7ce5904fff82131a4b73e5b9f9ca
Renat97 <renat97@gmail.com>
2020-11-13 19:31:52 -0800

    using react style components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
+const Title = styled.h1`font-size: 32px;
+line-height: 36px;
+font-weight: 700;
+color: #000;
+text-align: left;
+position: relative;
+left: 20px;
+top: -10px;
+font-family: Poppins;
+margin-block-start: 0.67em;
+margin-block-end: 0.67em;
+margin-inline-start: 0px;
+margin-inline-end: 0px;`;
+
+const Rating = styled.span`display: inline-block;
+position: relative;
+bottom: 32px;
+right: 19px;`;
+
+const Reviews = styled.h4`position: relative;
+left: 100px;
+bottom: 50px;
+display:inline-block;
+float:left;`;
+
+
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      reviewCount: 0
+      reviewCount: ''
     }
   }
   componentDidMount() {
@@ -13,17 +50,18 @@ class App extends React.Component {
       (response) => {
         console.log('response', response);
         this.setState({
-         reviewCount: response.data[0].reviewCount
+         reviewCount: response.data[0].reviewCount.toLocaleString('en')
         })
       }
     )
   }
-
   render() {
     return (
-      <div>
-        <h1>Hello World!</h1>
-        <h1>{this.state.reviewCount}</h1>
+      <div className="component">
+        <Title>Winchester Mystery House </Title>
+        <Rating> hello </Rating>
+        <Reviews>{this.state.reviewCount} </Reviews>
+        <Button>Im a button</Button>
       </div>
     );
   }
